c8d62bec90fb44801cced8f2d21d15124e1f9290
Victor Huynh <victor.h.huynh@gmail.com>
2019-08-22 22:52:00 +0000

    Let, const, arrow function, foreach, map

--- es6Refresher/main.js
+++ es6Refresher/main.js
@@ -5,13 +5,45 @@
 // ================================================================================== //
 
 // CONST AND LET
+// let name = 'John';
+// let test;
+
+// name = 'Jack';
+
+// const person = {
+//   name: 'John',
+//   age: 33
+// };
+
+// person.name = 'Jack';
+
+// const nums = [1,2,3,4];
+// nums.push(5);
+
+// console.log(nums);
 
 // ARROW FUNCTIONS
 
+// function sayHello() {
+//   console.log('Hello');
+// }
+
+const sayHello = (name) => {
+  console.log(`Hello ${name}`);
+};
+
+const fruits = ['Apples', 'Oranges', 'Grapes'];
+
+// sayHello('Victor');
+
 // FOREACH
+// fruits.forEach((fruit, index) => console.log(fruit))
 
 // MAP
 
+const singleFruit = fruits.map((fruit) => fruit.slice(0, -1));
+
+console.log(singleFruit);
 // FILTER
 
 // SPREAD
