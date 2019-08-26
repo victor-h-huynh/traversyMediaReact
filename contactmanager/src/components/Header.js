104fc5a5dbfe6c1b831d56ae7140cd7653405c89
Victor Huynh <victor.h.huynh@gmail.com>
2019-08-26 22:58:33 +0000

    styling const

--- contactmanager/src/components/Header.js
+++ contactmanager/src/components/Header.js
@@ -5,7 +5,7 @@ const Header = (props) => {
   const { branding } = props
   return (
     <div>
-      <h1 style={{ color: 'red', fontSize: '50px' }}>{branding}</h1>
+      <h1 style={headingStyle}>{branding}</h1>
     </div>
   );
 };
@@ -16,6 +16,11 @@ Header.defaultProps = {
 
 Header.propTypes = {
   branding: PropTypes.string.isRequired
+}
+
+const headingStyle = {
+  color: 'royalblue', 
+  fontSize: '50px'
 }
 
 export default Header
