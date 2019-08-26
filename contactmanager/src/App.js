5ec0ddb8aa38740d560254eec93d1540276cc6de
Victor Huynh <victor.h.huynh@gmail.com>
2019-08-26 22:52:08 +0000

    created index, app, header, contact

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Contact from './components/Contact';
+import Header from './components/Header';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header branding="Contact Manager"/>
+        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555"/>
+        <Contact name="Karen Smith" email="karen@gmail.com" phone="333-333-3333"/>
       </div>
     );
   }
---
+++ contactmanager/src/components/Contact.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+
+
+class Contact extends Component {
+  render() {
+    const {name, email, phone } = this.props;
+    return (
+      <div>
+        <h4>{name}</h4>
+        <ul>
+          <li>Email: {email}</li>
+          <li>Phone: {phone}</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  phne: PropTypes.string.isRequired
+}
+
+export default Contact;
