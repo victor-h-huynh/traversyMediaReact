dd7bd026df21e1ecaa129147e4fb11f92a7bc9cf
Victor Huynh <victor.h.huynh@gmail.com>
2019-08-26 19:58:42 +0000

    Classes, Subclasses, Modules

--- es6Refresher/main.js
+++ es6Refresher/main.js
@@ -77,25 +77,73 @@
 
 // DESTRUCTURING
 
-const animal = {
-  breed: 'dog',
-  traits: {
-    color: ['yellow', 'red'],
-    legs: 4
-  },
-  tail: true
-}
+// const animal = {
+//   breed: 'dog',
+//   traits: {
+//     color: ['yellow', 'red'],
+//     legs: 4
+//   },
+//   tail: true
+// }
 
-const { breed } = animal;
-const { color } = animal.traits
-const { tail } = animal
+// const { breed } = animal;
+// const { color } = animal.traits
+// const { tail } = animal
 
-console.log(breed)
-console.log(color[1])
-console.log(tail)
+// console.log(breed)
+// console.log(color[1])
+// console.log(tail)
 
 // CLASSES
 
+// class Animal {
+//   constructor(name, species, cry) {
+//     this.name = name;
+//     this.species = species;
+//     this.cry = cry;
+//   }
+
+//   greet() {
+//     return `I am a ${this.species} ${this.cry}`
+//   }
+  
+
+// }
+
+// const animal1 = new Animal('Snoopy', 'Dog', 'Woof');
+// const animal2 = new Animal('Santa Paws', 'Cat', 'Meow');
+
+// console.log(animal1.greet());
+// console.log(animal2.greet());
+
 // SUBCLASSES
 
+// class Customer extends Animal {
+//   constructor(name, species, cry, balance) {
+//     super(name, species, cry)
+//     this.balance = balance
+//   }
+
+//   info() {
+//     return `${this.name} owes $${this.balance}.00 :c ${this.cry}`
+//   }
+// }
+
+
+// const customer1 = new Customer('Snoopy', 'Dog', 'Woof', 100)
+// const customer2 = new Customer('Snakey', 'Snake', 'Hiss', 420)
+// console.log(customer1.info());
+// console.log(customer2.info());
+
 // MODULES
+
+// file 1 (file1.js)
+
+// export const name = 'Jeff';
+// export const nums = [1,2,3];
+// export default Person;
+
+// // file 2 (file2.js)
+
+// import { name, nums } from './file1';
+// import Person from '.file1';
