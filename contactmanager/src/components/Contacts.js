7257a015f18eb366f550dcb7f01525631e0560a0
Victor Huynh <victor.h.huynh@gmail.com>
2019-09-05 18:20:51 +0000

    commit

--- contactmanager/src/components/Contacts.js
+++ contactmanager/src/components/Contacts.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Contact from "./Contact";
 
 class Contacts extends Component {
   constructor() {
@@ -33,7 +34,11 @@ class Contacts extends Component {
     return (
       <div>
         {contacts.map(contact => (
-          <h1>{contact.name}</h1>
+          <Contact
+            name={contact1.name}
+            email={contact.email}
+            phone={contact.phone}
+          />
         ))}
       </div>
     );
